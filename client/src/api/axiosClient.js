c30ac596be3179a1a84bd88ea8343c399d605b5f
Dhavamani6804 <sdhavamani22@gmail.com>
2026-07-09 23:13:36 +0530

    implemented dashboard and profile module

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -7,4 +7,17 @@ const axiosClient = axios.create({
     },
 });
 
+axiosClient.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem("token");
+
+    console.log("JWT Token:", token);
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
 export default axiosClient;
