e3d1ff84b76b86246b6b63c52f3c196fd2b5e953
Dhavamani6804 <sdhavamani22@gmail.com>
2026-07-15 18:04:31 +0530

    completed tailoring resume module

---
+++ backend/src/main/java/com/careeros/service/ResumeTailorService.java
@@ -0,0 +1,11 @@
+package com.careeros.service;
+
+import com.careeros.dto.response.ResumeTailorResponse;
+
+public interface ResumeTailorService {
+
+    ResumeTailorResponse tailorResume(
+    String applicationId
+    );
+
+}
